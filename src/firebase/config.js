4f4d38da93cc6de9930d4de41875fd21bad9b87b
promptforgeoficialai-dev <promptforge.oficial.ai@gmail.com>
2026-07-14 17:24:06 -0500

    Configuración de Firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,25 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsohm8pMZ8AosW6JNnfYERGvh40qHCkvk",
+  apiKey: "AIzaSyASohm8pMZ8AosW6JNnfYERGvh4OqHCkvk",
   authDomain: "promptforgeoficial-92478.firebaseapp.com",
   projectId: "promptforgeoficial-92478",
   storageBucket: "promptforgeoficial-92478.firebasestorage.app",
   messagingSenderId: "639060004391",
-  appId: "1:639060004391:web:533a54161ef62601238802",
-  measurementId: "G-Y643D25EY7"
+  appId: "1:639060004391:web:c501b28306728a9d238802",
+  measurementId: "G-77N1N1E82H"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exportaciones para el proyecto
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+export { analytics };
+export default app;
